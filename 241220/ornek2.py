ebe5d08e9d4910f108979169b3d23be6f33da229
Canan77745 <kralcanan42@gmail.com>
2024-12-20 12:05:03 +0300

    Add files via upload

---
+++ 241220/ornek2.py
@@ -0,0 +1,6 @@
+kisakenar=int(input("Kisa kenari giriniz:"))
+uzunkenar=int(input("Uzun kenari giriniz:"))
+alan=(kisakenar*uzunkenar)
+cevre=(kisakenar+uzunkenar)*2 
+print=("dıkdortgenın cevresı :",cevre)
+print=("dıkdortegenın alanı:",alan)
---
+++ 241220/ornek3.py
@@ -0,0 +1,3 @@
+sayi=int(print("Bir sayı giriniz"))  #kullanıcıdan sayı alınız.
+islem=sayi*sayi                      #kullanıcıdan alınan sayının karesini alınız.
+print=("Sayısının karesi",islem)     #kenara çıktısını yazdırınız.
